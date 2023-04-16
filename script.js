538272f3813fbd7cd863a86db8208cbe2b17546b
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-17 01:19:56 +0200

    Practising forEach()

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('funguju!');
+
+
+const ovciJmena = [
+  'Bětuška',
+  'Cína',
+  'Dolly',
+  'Heidy',
+  'Líza',
+  'Belinda',
+  'Celia',
+  'Elvíra',
+  'Karla',
+  'Otýlie',
+  'Škraboška',
+  'Kopretinka',
+  'Berta',
+  'Růženka',
+  'Bobinka',
+  'Žofka',
+  'Dášenka',
+  'Vlnka',
+  'Květuše',
+  'Pampeliška',
+];
+
+ovciJmena.forEach((jmeno, index) => {
+  document.body.innerHTML += `<p>Ovecka ${jmeno} jako ${index + 1}.preskocila pres plot.</p><br> `
+});
